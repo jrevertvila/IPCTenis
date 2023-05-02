29908d830c3f19bf3c306f79bd9a13c70c5b1961
jrevertvila <jrevertvila@gmail.com>
2023-05-02 18:35:59 +0200

    cambiar nombres de ficheros

--- src/javafxmlapplication/controller/InicioController.java
+++ src/javafxmlapplication/controller/InicioController.java
@@ -4,27 +4,17 @@
  */
 package javafxmlapplication.controller;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import javafxmlapplication.controller.RegisterController;
-import model.ClubDAOException;
 
 /**
  * FXML Controller class
  *
- * @author joel
+ * @author joeli
  */
 public class InicioController implements Initializable {
 
@@ -38,50 +28,15 @@ public class InicioController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-                
+        // TODO
     }    
 
     @FXML
-    private void loadLogin(ActionEvent event) throws IOException, ClubDAOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/Login.fxml"));
-        Parent root = loader.load();
-
-        Scene scene = new Scene(root);
-        Stage stage = new Stage();
-        LoginController controladorLogin = loader.getController();
-       
-        stage.setScene(scene);
-        stage.setTitle("Iniciar Sesión");
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.showAndWait();
-        
-        
-        
-        if (controladorLogin.isLogged()) {
-            System.out.println("ENTRA EN IS LOGGEDD");
-            FXMLLoader loader2 = new FXMLLoader(getClass().getResource("../view/Dashboard.fxml"));
-            Parent root2 = loader2.load();
-            javafxmlapplication.JavaFXMLApplication.setRoot(root2);
-        }
+    private void loadLogin(ActionEvent event) {
     }
 
     @FXML
-    private void loadRegister(ActionEvent event) throws IOException {
-        FXMLLoader loader= new FXMLLoader(getClass().getResource("../view/Register.fxml"));
-        Parent root = loader.load();
-
-        Scene scene = new Scene(root);
-        Stage stage = new Stage();
-        RegisterController controladorRegister = loader.getController();
-        stage.setScene(scene);
-        stage.setTitle("Registrarse");
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.showAndWait();
-        
+    private void loadRegister(ActionEvent event) {
     }
-    
-    
-
-
     
 }
