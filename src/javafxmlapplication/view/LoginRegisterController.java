6048535378110540cf47890e6a0c16154300ff6e
jrevertvila <jrevertvila@gmail.com>
2023-05-21 01:50:21 +0200

    Mejorar interfaz register y login

--- src/javafxmlapplication/view/LoginRegisterController.java
+++ src/javafxmlapplication/view/LoginRegisterController.java
@@ -48,10 +48,10 @@ public class LoginRegisterController implements Initializable {
         Object o = e.getSource();
         Button b = (Button) o;
         if (b.getId().equals("iniSesionBtnNav")){
-            iniSesionBtnNav.getStyleClass().add("activeLR");
+            if (!iniSesionBtnNav.getStyleClass().contains("buttonSidebarActive")) iniSesionBtnNav.getStyleClass().add("activeLR");
             registerBtnNav.getStyleClass().remove("activeLR");
         } else {
-            registerBtnNav.getStyleClass().add("activeLR");
+            if (!registerBtnNav.getStyleClass().contains("buttonSidebarActive")) registerBtnNav.getStyleClass().add("activeLR");
             iniSesionBtnNav.getStyleClass().remove("activeLR");
         }
     }
